57aa362ae15817d68d5dff0d65d1c4e322cd680d
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2021-10-26 23:23:43 -0300

    packages install and packages load commands

--- basics language structures.r
+++ basics language structures.r
@@ -1,8 +1,33 @@
+# install packages
+install.packages("stringr")
+install.packages("ggplot2", "dplyr")
+install.packages('jsonlite', dependencies=TRUE)
+
+# load packages
+library(stringr)
+library(ggplot2)
+library(jsonlite)
+
+
 # variables
-mystring <- "Hello World!"
+mystring = "Hello World!"
 mynumber <- 55
+(mynumber + 170) -> newnumber
+
+
+name <- "John"
+lastname <- "Smith"
+#fullname <- c(name, lastname)
+#?c to show c function help
+fullname <- str_c(name, " ", lastname)
+print(summary(newnumber))
+
 
 # output
-print(mynumber)
+print(newnumber)
 print(mystring)
+print(fullname)
+
+# built-in functions
+quit("no")
 
